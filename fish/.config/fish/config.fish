c6f748f5231fdca4e1ca5d140f1e9953d7ade19d
beanrob <contact.beanrob@gmail.com>
2025-03-02 22:27:16 +0000

    removed fixed hostname

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -32,8 +32,6 @@ end
 # }}}
 
 # VARIABLES ------------------------------------------------------------------------------- {{{
-export HOSTNAME="thonkbook-neo"
-
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_STATE_HOME=$HOME/.local/state
