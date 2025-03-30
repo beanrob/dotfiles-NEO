9431bab91dee9b81c347b4d0759e958791671724
beanrob <contact.beanrob@gmail.com>
2025-03-30 17:43:38 +0100

    added hostname

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,6 +36,8 @@ end
 # }}}
 
 # VARIABLES ------------------------------------------------------------------------------- {{{
+export HOSTNAME="thonkbook-neo"
+
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_STATE_HOME=$HOME/.local/state
